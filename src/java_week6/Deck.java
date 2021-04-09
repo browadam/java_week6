a2880e308176b54b4fa3a64c8e1b268a508fedb4
browadam <browadam@users.noreply.github.com>
2021-04-09 08:58:01 -0600

    Initial commit

---
+++ src/java_week6/Deck.java
@@ -0,0 +1,24 @@
+package java_week6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Deck extends Card{
+	
+	List <Card> cards = new ArrayList<Card>();
+	
+	public Deck (ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+	
+	public void shuffle() {
+		
+	}
+	
+	public Card draw() {
+		
+	}
+	
+	
+}
